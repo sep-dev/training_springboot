7d65829dbb67b9e101d1192d8249ff12b1504d7a
sep-matsukane <matsukane@se-project.co.jp>
2019-05-10 16:08:57 +0900

    登録・削除処理

--- src/main/java/com/jpasample/EmpController.java
+++ src/main/java/com/jpasample/EmpController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -17,13 +19,38 @@ public class EmpController {
     public String index(Model model) {
 		
 		// html テンプレート上 ${変数名}にセットされる。
-        model.addAttribute("title", "Spring-Boot サンプルぞ。");
+        model.addAttribute("title", "社員リスト");
 
+        // 既に存在するEmployeeデータ取得
         List<Employee> empList=empRepository.findAll();
     	model.addAttribute("emplist", empList);
-        
+
+    	// 新規登録用Employee
+    	Employee new_employee = new Employee();
+    	model.addAttribute("new_emp", new_employee);
+    	
         // src/main/resources/templates/ 下の HTML を出力。
         // この場合は index.html となる。
         return "index";
     }
+
+	// 新規登録処理。画面入力 empname をセットした Employee オブジェクトを EmployeeRepository に渡し新規追加
+    @RequestMapping(value="/emp/regist", method=RequestMethod.POST)
+    public String regist(@ModelAttribute Employee new_employee, Model model) {
+
+    	empRepository.save(new_employee);
+
+    	// 登録後社員一覧にリダイレクト
+        return "redirect:/emp";
+    }
+
+    // URLパス上の id に一致する Employee データを削除
+    @RequestMapping(value="/emp/{id}/delete", method=RequestMethod.DELETE)
+    public String delete(@PathVariable Long id, Model model) {
+    	
+    	empRepository.deleteById(id);
+
+        return "redirect:/emp";
+    }
+    
 }
